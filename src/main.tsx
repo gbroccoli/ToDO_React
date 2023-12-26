a1ec4d7e0f18b44c6e7410556853e27201463ee0
Broccoli <vovo.r@ya.ru>
2023-12-27 00:32:21 +1000

    edit

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import {
-  createBrowserRouter, RouterProvider
+  createBrowserRouter, RouterProvider, useNavigate
 } from  "react-router-dom"
 import './index.css'
 import Login from './pages/login/Login.tsx'
 
+
+const isAuth = (): boolean => {
+  const cokiesValue = document.cookie
+  .split(";")
+  .find((row)=>row.startsWith("access_token"))
+
+  return cokiesValue !== undefined
+
+}
+
+const RedicToProfile: React.FC = () => {
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+    if (isAuth()) {
+      navigate("/dash")
+    }
+  }, [])
+}
+
 const routers = createBrowserRouter([
   {
     path: "/",
-    element: <App/>
+    element: (
+      <>
+        <RedicToProfile/>
+        <Login/>
+      </>
+    )
   },
-  {
-    path: "/login",
-    element: <Login/>
-  }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
